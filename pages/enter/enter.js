5b7c2675035018e4611cf827b892327fd77ba817
zh_dev <hao1993624@gmail.com>
2017-09-05 02:19:19 +0800

    complete 'enter' page

--- pages/enter/enter.js
+++ pages/enter/enter.js
@@ -17,32 +17,7 @@ Page({
   },
 
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    
   },
 
 //跳转到日期计算界面
@@ -51,13 +26,12 @@ Page({
       url: '../datecalculate/datecalculate',
     })
   },
-
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  jumpToWell:function(){
+    wx.showModal({
+      title: '提示',
+      content: '敬请期待~',
+      showCancel: false,
+      confirmText: "OK~"
     })
   }
 })
